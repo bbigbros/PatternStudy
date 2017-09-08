10d5576af24fe3eaeb28013e8e04506ea564d3f2
Jaeryong Im <steveim1201@gmail.com>
2017-09-08 12:49:31 +0900

    DuckActionClasses 분ㄴ리 * QuackAction과 FlyAction으로 분리

---
+++ Pattern/DuckFlyActionClasses.cs
@@ -0,0 +1,21 @@
+﻿using Pattern.ICollection;
+using System;
+
+namespace Pattern.CAction.Flys
+{
+    class DuckNonFly : IFlyBehavior
+    {
+        public void Fly()
+        {
+            Console.WriteLine("I dot't flying...");
+        }
+    }
+
+    class DuckSlowFly : IFlyBehavior
+    {
+        public void Fly()
+        {
+            Console.WriteLine("slow flying...");
+        }
+    }
+}
---
+++ Pattern/DuckQuackActionClasses.cs
@@ -0,0 +1,21 @@
+﻿using Pattern.ICollection;
+using System;
+
+namespace Pattern
+{
+    class DuckMuteQuack : IQuackBehavior
+    {
+        public void Quack()
+        {
+            Console.WriteLine(".....");
+        }
+    }
+
+    class DuckCrazyQuack : IQuackBehavior
+    {
+        public void Quack()
+        {
+            Console.WriteLine("alwejrklajsdflkjsaifsd");
+        }
+    }
+}
