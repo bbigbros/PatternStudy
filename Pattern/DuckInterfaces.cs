90f644480e2b981e35b1aad10676d6bafd3c84f3
Jaeryong Im <steveim1201@gmail.com>
2017-09-08 11:27:06 +0900

    Duck fly, quack 인터페이스 추가

---
+++ Pattern/DuckInterfaces.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pattern.ICollection
+{
+    interface IFlyBehavior
+    {
+        void Fly();
+    }
+
+    interface IQuackBehavior
+    {
+        void Quack();
+    }
+}
