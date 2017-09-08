920ba4d8ec6e206c732ced6d8f4c145ee481eda3
Jaeryong Im <steveim1201@gmail.com>
2017-09-08 09:56:49 +0900

    Pattern 공부를 위한 첫번째 커밋
    * Strategy Pattern을 위한 base 코드

---
+++ Pattern/Duck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pattern
+{
+    class Duck
+    {
+        public void fly()
+        {
+            Console.WriteLine("Duck is flying....");
+        }
+        public void quack()
+        {
+            Console.WriteLine("Quack Quack!!");
+        }
+        public void swim()
+        {
+            Console.WriteLine("Duck is swimming..");
+        }
+        public void display()
+        {
+            Console.WriteLine("Duck is white!");
+        }
+    }
+
+    class RedDuck : Duck
+    {
+    }
+}
---
+++ Pattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Pattern;
+
+
+namespace Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Pattern ");
+            Console.WriteLine("--------------------------");
+
+            RedDuck rd = new RedDuck();
+            rd.display();
+        }
+    }
+}
