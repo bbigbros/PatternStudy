cce9b23358aadc18199a6a63363881b615bd3c29
Jaeryong Im <steveim1201@gmail.com>
2017-09-08 11:27:46 +0900

    Duck 행동에 관한 클래스 집합 set 추가

---
+++ Pattern/DuckActionClasses.cs
@@ -0,0 +1,21 @@
+﻿using Pattern.ICollection;
+using System;
+
+namespace Pattern.CActions
+{
+    class DuckNonFly : IFlyBehavior
+    {
+        public void Fly()
+        {
+            Console.WriteLine("I dot't flying...");
+        }
+    }
+
+    class DuckSlowFly : IFlyBehavior
+    {
+        public void Fly()
+        {
+            Console.WriteLine("slow flying...");
+        }
+    }
+}
